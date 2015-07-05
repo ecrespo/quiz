cbe9a99306e4c1aeb5a47ca56e3c2b48e07f57ae
Ernesto Nadir Crespo Avila <ecrespo@gmail.com>
2015-07-04 23:35:12 -0430

    Creada página de los créditos de la aplicación

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req,res) {
+	res.render('author', { title: 'Créditos', autor:'Ernesto Crespo',email:'ecrespo@gmail.com', blog:'blog.crespo.org.ve',foto:'/images/ecrespo.jpg'});
+
+});
+
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
 
