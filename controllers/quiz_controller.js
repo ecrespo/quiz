011663bcf1db93fca5a9671b99afc3f24f18e8ea
Ernesto Nadir Crespo Avila <ecrespo@gmail.com>
2015-07-08 22:44:02 -0430

    Arreglado el despliegue

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,49 +1,19 @@
 var models = require('../models/models.js');
 
-
-exports.question = function (req,res) {
-	models.Quiz.findAll().success(function(quiz) {
-		res.render('quizes/question', {pregunta:quiz[0].pregunta})
-
-	})
-	
-};
-
-
-exports.answer = function (req,res) {
-	models.Quiz.findAll().success(function(quiz) {
-		if (req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {respuesta:'Correcto'});
-
-		} else {
-			res.render('quizes/answer', {respuesta:'Incorrecto'});
-
-	}	
-	})
-	
-	
-};
-
-<<<<<<< HEAD
-// GET /quizes/:id
-exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: quiz});
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
   })
 };
 
-// GET /quizes/:id/answer
+// GET /quizes/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', 
-                 { quiz: quiz, respuesta: 'Correcto' });
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
     } else {
-      res.render('quizes/answer', 
-                 { quiz: quiz, respuesta: 'Incorrecto'});
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
     }
   })
 };
-
-=======
->>>>>>> parent of 5aa7115... Despliegue en heroku
