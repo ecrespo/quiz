be8f653541ea9a55a2bd95664a989f2c69e316bb
Ernesto Nadir Crespo Avila <ecrespo@gmail.com>
2015-07-08 23:42:11 -0430

    Arreglado los creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+router.get('/author', function(req,res) {
+	res.render('author', { title: 'Créditos', autor:'Ernesto Crespo',email:'ecrespo@gmail.com', blog:'blog.crespo.org.ve',foto:'/images/ecrespo.jpg'});
+
+});
+
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
