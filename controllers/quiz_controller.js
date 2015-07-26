035ea249424edcceb6464b168e57c496660578f6
Ernesto Nadir Crespo Avila <ecrespo@gmail.com>
2015-07-26 18:34:26 -0430

    Agregada la busqueda en la página que lista las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,10 +16,22 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
+  if (!req.params.search) {
+    models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes});
+    }).catch(function(error) { next(error);})
+  } else {
+    //findAll({where: ["pregunta like ?", search]}]
+    models.Quiz.findAll({
+      where: {
+        pregunta: {
+          $like: "%"+req.params.search + "%"
+        }
+      }
+    }).then(function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes});
+    }).catch(function(error) { next(error);})
   }
-  ).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
